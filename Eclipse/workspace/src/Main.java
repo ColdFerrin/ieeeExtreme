9ef9560001a0c2897ddbd14fde5286de33e3cafc
danielstruck <struckd@my.erau.edu>
2018-10-20 06:45:40 -0700

    finished infinite snakes and ladders

--- Eclipse/workspace/src/Main.java
+++ Eclipse/workspace/src/Main.java
@@ -5,269 +5,83 @@ import java.io.*;
 
 // Please name your class Main
 class Main {
-	static Scanner sc = new Scanner(getInputStream());
-	static InputStream getInputStream() {
-		try {
-			return new FileInputStream(new File("map.txt"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return System.in;
-	}
-	static GameMap map;
+	static int[][] tipping;
+	static int numLetters;
+	static int weightings;
 	
-	static int count = 0;
-	public static void main(String args[]) {
-		int height = sc.nextInt();
-		int width = sc.nextInt();
-		map = new GameMap(width, height);
-		sc.nextLine();
+	
+	public static void main (String[] args) throws java.lang.Exception {
+	    Scanner sc = new Scanner(System.in);
+		String[] initVals = sc.nextLine().split(",");
+		numLetters = Integer.parseInt(initVals[0]);
+		weightings = Integer.parseInt(initVals[1]);
 		
+		tipping = new int[weightings][numLetters];
 		
-		for (int row = 0; row < 2 * height + 1; row++) {
-			String line = sc.nextLine();
-			for (int col = 0; col < line.length(); col++) {
-				map.setChar(col, row, line.charAt(col));
+		for (int i = 0; i < weightings; i++) {
+			String[] sample = sc.nextLine().split("-");
+			adjustTipping(sample, i);
+		}
+		
+printArr(tipping);
+
+		StringBuilder b = new StringBuilder();
+		for (int var1 = 0; var1 < numLetters - 1; var1++) {
+			for (int var2 = var1 + 1; var2 < numLetters; var2++) {
+				for (int var3 = var2; var3 < numLetters; var3++) {
+					for (int var4 = var3 + 1; var4 < numLetters; var4++) {
+						if (testDoubleTippings(var1, var2, var3, var4)) {
+							char c1 = (char) (var1 + 'A'),
+								 c2 = (char) (var2 + 'A'),
+								 c3 = (char) (var3 + 'A'),
+								 c4 = (char) (var4 + 'A');
+							b.append(c1 + "" + c2 + "=" + c3 + "" + c4);
+						}
+					}
+				}
 			}
 		}
-		// ==end input==
-//		Cell start = map.findPath(new Cell(5, 13), new Cell(9, 5));
-////		while (start.cameFrom != null) {
-////			System.out.println(start.y + ", " + start.x);
-////			start = start.cameFrom;
-////		}
-//		ArrayList<Cell> path = reversePath(start);
-//		for (int i = 1; i < path.size(); i++) {
-//			chooseOption(path.get(i), path.get(i-1));
-//		}
-			
+		System.out.println(b.toString());
+	}
+	
+	
+	static boolean testDoubleTippings(int var1, int var2, int var3, int var4) {
+		for (int i = 0; i < weightings; i++) {
+			int sum12 = tipping[i][var1] + tipping[i][var2];
+			int sum34 = tipping[i][var3] + tipping[i][var4];
+			if (sum12 != sum34)
+				return false;
+		}
 		
-		
-		
-		Cell start = map.player;
-		
-		boolean shouldBreak = false;
-		while (!shouldBreak && map.cherryLocations.size() > 0) {
-			Cell current = findShortest(start);
-			start = current;
-////map.setChar(current.x, current.y, ("" + count++).charAt(0));
-			ArrayList<Cell> path = reversePath(current);
-			shouldBreak = choosePathOptions(path);
-//printMap();
+		return true;
+	}
+	static void adjustTipping(String[] sample, int weightingNumber) {
+		for (char c: sample[0].toCharArray()) {
+			tipping[weightingNumber][c - 'A'] = -1;
+		}
+		for (char c: sample[1].toCharArray()) {
+			tipping[weightingNumber][c - 'A'] = 1;
 		}
 	}
-	static boolean choosePathOptions(ArrayList<Cell> path) {
-		for (int i = 2; i < path.size(); i += 2)
-			if (chooseOption(path.get(i), path.get(i-2)))
-				return true;
-		return false;
-	}
-	static ArrayList<Cell> reversePath(Cell current) {
-		ArrayList<Cell> path = new ArrayList<Cell>();
-		
-		do {
-			path.add(0, current);
-			current = current.cameFrom;
-		} while (current != null);
-		
-		return path;
-	}
-	static Cell findShortest(Cell start) {
-		Cell shortest = null;
-		int cherryIndex = 0;
-		
-		for (int i = 0; i < map.cherryLocations.size(); i++) {
-			Cell cherry = map.cherryLocations.get(i);
-			Cell current = map.findPath(start, cherry);
-			if (current != null && (shortest == null || shortest.startDist > current.startDist)) {
-				shortest = current;
-				cherryIndex = i;
-			}
-		}
-		
-		map.cherryLocations.remove(cherryIndex);
-		
-		return shortest;
-	}
-	static boolean chooseOption(Cell current, Cell previous) {
-		int distX = current.distanceX(previous);
-		int distY = current.distanceY(previous);
-		char choice;
-		
-		if (distX > 0) {
-			choice = 'R';
-		}
-		else if (distX < 0) {
-			choice = 'L';
-		}
-		else if (distY > 0) {
-			choice = 'D';
-		}
-		else if (distY < 0) {
-			choice = 'U';
-		}
-		else {
-			choice = 'W';
-		}
-		
-		System.out.println(choice);
-		
-		if (sc.hasNextLine())
-			return sc.nextLine().endsWith("X");
+	
+	static int factorial(int number) {
+		if (number == 2)
+			return 2;
 		else
-			return false;
+			return number * factorial(number - 1);
 	}
 	
 	
-	static void printMap() {
-		for (char[] row: map.charCells) {
-			for (char cell: row)
-				System.out.print(cell);
+	
+	static void printArr(int[][] arr) {
+		for (int i = 0; i < arr[0].length; i++)
+			System.out.format("  %c ", (char) ('A' + i));
+		System.out.println();
+		
+		for (int[] row: arr) {
+			for (int i: row)
+				System.out.format("%3d ", i);
 			System.out.println();
 		}
-	}
-}
-
-class GameMap {
-	char[][] charCells;
-	ArrayList<Cell> cherryLocations = new ArrayList<Cell>();
-	Cell player;
-	
-	
-	public GameMap(int width, int height) {
-		charCells = new char[2 * height + 1][2 * width + 1];
-		
-		for (int y = 0; y < charCells.length; y++) {
-			for (int x = 0; x < charCells[0].length; x++) {
-				setChar(x, y, '_');
-			}
-		}
-	}
-	
-	
-	public void setChar(int x, int y, char c) {
-		charCells[y][x] = c;
-		
-		if (c == '@')
-			cherryLocations.add(new Cell(x, y));
-		else if (c == '1')
-			player = new Cell(x, y);
-	}
-	public char getChar(int x, int y) {
-		return charCells[y][x];
-	}
-
-	int count = 0;
-	public Cell findPath(Cell start, Cell target) {
-		int height = charCells.length,
-			width = charCells[0].length;
-
-		ArrayList<Cell> closed = new ArrayList<Cell>();
-		ArrayList<Cell> open = new ArrayList<Cell>();
-		Cell[][] cells = new Cell[height][width];
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				cells[y][x] = new Cell(x, y);
-				if (getChar(x, y) != '#')
-					open.add(cells[y][x]);
-				else
-					closed.add(cells[y][x]);
-			}
-		}
-		
-		cells[start.y][start.x].gScore = 0;
-		cells[start.y][start.x].fScore = start.distance(target);
-		cells[start.y][start.x].startDist = 0;
-//System.out.println(">>From " + start.y + ", " + start.x + " to " + target.y + ", " + target.x);
-		while (!open.isEmpty()) {
-			Cell current = lowestFScore(open);
-			
-//System.out.println(current.x + "," + current.y + " | " + start.x + "," + start.y + " | " + current.fScore);
-//setChar(current.x, current.y, ("" + count++).charAt(0));
-			
-			if (current.distance(target) == 0) {
-//System.out.println("Return " + current.y + ", " + current.x);
-				return current;
-			}
-			
-			open.remove(current);
-			closed.add(current);
-			
-			int x = current.x;
-			int y = current.y;
-			ArrayList<Cell> neighborArray = new ArrayList<Cell>(4);
-			if (y < cells[0].length - 1)
-				neighborArray.add(cells[y + 1][x]);
-			if (y > 0)
-				neighborArray.add(cells[y - 1][x]);
-			if (x < cells.length - 1)
-				neighborArray.add(cells[y][x + 1]);
-			if (x > 0)
-				neighborArray.add(cells[y][x - 1]);
-			
-			
-			for (Cell neighbor: neighborArray) {
-//if (start.y == 13 && start.x == 5 && target.y == 5 && target.x == 9)
-//System.out.println("Neighbor: " + current.y + "/" + neighbor.y + ", " + current.x + "/" + neighbor.x + " | " + neighbor.fScore + " " + getChar(neighbor.x, neighbor.y));
-				if (closed.contains(neighbor)) {
-//if (start.y == 13 && start.x == 5 && target.y == 5 && target.x == 9)
-//System.out.println("Skipped");
-					continue;
-				}
-//if (current.distance(neighbor) > 1)
-//	System.out.println(">>ERROR: " + neighbor.y + "," + neighbor.x + " | " + current.y + ", " + current.x);
-				
-				int gScore = current.gScore + current.distance(neighbor);
-				
-				if (!open.contains(neighbor))
-					open.add(neighbor);
-				else if (gScore >= neighbor.gScore)
-					continue;
-				
-//System.out.println("add " + neighbor.y + "," + neighbor.x);
-				neighbor.cameFrom = current;
-				neighbor.gScore = gScore;
-				neighbor.fScore = gScore + neighbor.distance(target);
-				neighbor.startDist = current.startDist + 1;
-			}
-		}
-		
-		return null;
-	}
-	private Cell lowestFScore(ArrayList<Cell> open) {
-		Cell lowest = open.get(0);
-		
-		for (int i = 1; i < open.size(); i++)
-			if (lowest.fScore > open.get(i).fScore)
-				lowest = open.get(i);
-		
-		return lowest;
-	}
-	
-}
-
-class Cell {
-	Cell cameFrom;
-	int gScore;
-	int fScore;
-	int startDist;
-	int x, y;
-	
-	public Cell(int x, int y) {
-		this.x = x;
-		this.y = y;
-
-		gScore = Integer.MAX_VALUE;
-		fScore = Integer.MAX_VALUE;
-	}
-	
-	public int distanceX(Cell other) {
-		return x - other.x;
-	}
-	public int distanceY(Cell other) {
-		return y - other.y;
-	}
-	public int distance(Cell other) {
-		return Math.abs(distanceX(other)) + Math.abs(distanceY(other));
 	}
 }
